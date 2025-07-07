4b5a4cd745901923a1a9f581e962b11d5c07f252
cryptic25glitch <millionairecryptic25@gmail.com>
2025-07-07 16:50:35 +0530

    mode trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import data_transformation_config
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -50,5 +53,9 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr, data_transformation.data_transformation_config.preprocessor_obj_file_path))
+
 
---
+++ src/components/model_trainer.py
@@ -0,0 +1,75 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    RandomForestRegressor,
+    GradientBoostingRegressor,
+    AdaBoostRegressor
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join('artifacts', 'model.pkl')
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+        try:
+            logging.info('Splitting training and testing data')
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+
+            models = {
+                'Linear Regression': LinearRegression(),
+                'Random Forest': RandomForestRegressor(),
+                'Decision Tree': DecisionTreeRegressor(),
+                'KNN': KNeighborsRegressor(),
+                'XGBoost': XGBRegressor(),
+                'CatBoost': CatBoostRegressor(verbose=0),
+                'Gradient Boosting': GradientBoostingRegressor(),
+                'AdaBoost': AdaBoostRegressor()
+            }
+
+            model_report = {}
+            for model_name, model in models.items():
+                logging.info(f'Training {model_name}')
+                model.fit(X_train, y_train)
+                y_pred = model.predict(X_test)
+                r2_square = r2_score(y_test, y_pred)
+                model_report[model_name] = r2_square
+                logging.info(f'{model_name} R2 Score: {r2_square}')
+
+            best_model_name = max(model_report, key=model_report.get)
+            best_model_score = model_report[best_model_name]
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No suitable model found with R2 score > 0.6")
+
+            logging.info(f'Best model: {best_model_name} with R2 score: {best_model_score}')
+            save_object(best_model, self.model_trainer_config.trained_model_file_path)
+
+            return best_model_name, best_model_score
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
