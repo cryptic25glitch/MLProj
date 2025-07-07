8c30918fcf64b90e3715561f4feb99190368d606
cryptic25glitch <millionairecryptic25@gmail.com>
2025-07-07 14:56:58 +0530

    data transformation

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from src.exception import CustomException
+import dill
+def save_object(obj, file_path):
+    """
+    Save an object to a file using pickle.
+    
+    Parameters:
+    obj: The object to save.
+    file_path: The path where the object will be saved.
+    """
+    import pickle
+    try:
+         dir_path = os.path.dirname(file_path)
+         os.makedirs(dir_path, exist_ok=True)
+         with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    
+    except Exception as e:
+        raise CustomException(e, sys) from e
+
